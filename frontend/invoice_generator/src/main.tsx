150ea2f8759eafdd5fe3781a01d652eb18810ff7
Rashmi Gupta <rashmi.gupta7018@gmail.com>
2024-06-03 11:34:44 +0530

    tt

--- frontend/invoice_generator/src/main.tsx
+++ frontend/invoice_generator/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import './index.css';
 import store from './redux/Store';
 
-const container = document.getElementById('root');
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 root.render(
